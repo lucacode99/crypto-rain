2052b2ef361c0d36b213e7faa0f81bede71ba4f0
Lucas Pfister <digitalservices@outlook.de>
2022-05-16 13:01:18 +0200

    basic setup started

--- game.js
+++ game.js
@@ -1,15 +1,46 @@
 
 const body = document.body
 const landingPageDiv = document.getElementById("landing-div")
+const ctx = document.getElementById('canvas').getContext('2d');
+
+// map 
+const backgroundImg = new Image();
+backgroundImg.src = '../Pictures/earth.jpg';
+
+//avatar
+const avatarImg = new Image();
+avatarImg.src = '../Pictures/player_one.jpg';
+//let avatarPositionX = 650;
+//let avatarPositionY = 600;
+//avatarWidth = 15;
+//avatarLength = 20;
 
 
+// functions
+function startGame () {
+landingPageDiv.classList.add("invisibility");
+ctx.drawImage(backgroundImg, 0, 0, canvas.width, canvas.height);
+launchAstronaut();
+}
+
+function launchAstronaut () {
+ctx.drawImage(avatarImg, 0, 0, 20, 20);
+console.log("working")
+}
+
+
+  
 
 
 
 window.addEventListener("load", () => {
     document.getElementById('start-button').onclick = () => {
-        console.log("ein funktionierender Start-Knopf")
-        landingPageDiv.classList.add("invisibility")
+        console.log("ein funktionierender Start-Knopf");   
+        startGame ();  
+                                          
+
+
+
       };
 
 
@@ -17,13 +48,17 @@ window.addEventListener("load", () => {
 
 })
 
-
-
 /*
-// insert canvas
 
-const body = document.body;
-const canvas = document.createElement("canvas")
-body.append(canvas)
+const img = new Image();
+imgScale = 640/480;
+img.onload = function() {
+  ctx.drawImage(img, 0, 0,150*imgScale,150);
+};
+ 
+img.src = 'https://static.pexels.com/photos/8700/wall-animal-dog-pet.jpg';
+
+
+
 
 */
