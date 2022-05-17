00b18cc83d72bd730e1544e7113d974ff6e998d5
Lucas Pfister <digitalservices@outlook.de>
2022-05-17 16:03:44 +0200

    shitcoins included

--- game.js
+++ game.js
@@ -50,7 +50,6 @@ class Bitcoin {
     this.space = bitcoinHeight;
     this.speed = bitcoinFallingSpeed;
     this.posX  = Math.floor(Math.random() * 1300)
-    // old but gold: this.posX  = bitcoinStartingValueX;
     this.posY  = bitcoinStartingValueY;
   }
 
@@ -64,12 +63,63 @@ class Bitcoin {
   }
 }
 
+// shitcoin
+
+const shitcoinImg = new Image();
+shitcoinImg.src = '../Pictures/shitcoin_aufkleber.jpg';
+let shitcoinWidth = 65;
+let shitcoinHeight = 65;
+let shitcoinFallingSpeed = 5;
+let shitcoinStartingValueX = Math.random() * 1300;
+let shitcoinStartingValueY = Math.random() - 5 * 100;
+let shitcoins = [];
+
+// multitude of falling shitcoins
+
+class Shitcoin {
+  constructor() {
+    this.width = shitcoinWidth;
+    this.space = shitcoinHeight;
+    this.speed = shitcoinFallingSpeed;
+    this.posX  = Math.floor(Math.random() * 1300)
+    this.posY  = shitcoinStartingValueY;
+  }
+    move () {
+    this.posY += this.speed;
+    }
+
+    randomStartingPoint() {
+      this.posX;
+      this.posY;
+    }
+}
+
+
+
 // -------------------- Section 02: functions and game logic -------------------- \\
+
+function curseOfTooManyShitcoins() {
+  const nextShitcoins = [];
+  shitcoins.forEach(shitcoin => {
+  if (avatarPositionX < shitcoin.posX + shitcoin.width && avatarPositionX + avatarWidth > shitcoin.posX && avatarPositionY < shitcoin.posY + shitcoin.space &&  avatarLength + avatarPositionY > shitcoin.posY)  {
+      lifeCounter -= 1;
+      console.log(lifeCounter);
+    }  else {
+      shitcoin.move(); 
+      if (shitcoin.posY < 700) {
+      nextShitcoins.push(shitcoin);
+      const {posY, posX, space, width} = shitcoin
+      ctx.drawImage(shitcoinImg, posX, posY, width, space);
+      }
+       }
+  })
+  shitcoins = nextShitcoins;
+}
+
 
 function blessingOfAMillionBitcoins() {
   const nextBitcoins = [];
   bitcoins.forEach(bitcoin => {
-  //const currentStartingPosition = bitcoin.randomStartingPoint();
   if (avatarPositionX < bitcoin.posX + bitcoin.width && avatarPositionX + avatarWidth > bitcoin.posX && avatarPositionY < bitcoin.posY + bitcoin.space &&  avatarLength + avatarPositionY > bitcoin.posY)  {
     bitcoinCounter +=1;
     console.log(bitcoinCounter);
@@ -109,9 +159,16 @@ function animate() {
   ctx.drawImage(backgroundImg, 0, 0, canvas.width, canvas.height);
   launchAstronaut();
   blessingOfAMillionBitcoins();
-  if (animationFrameId % 200 === 0) {
+  curseOfTooManyShitcoins();
+  if (animationFrameId % 200 === 0) {   
    bitcoins.push(new Bitcoin());
   } 
+  if (animationFrameId % 500 === 0) {   
+    shitcoins.push(new Shitcoin());
+  }
+  if (lifeCounter === 0) {
+    gameOver = true;
+  }
   if (gameOver) {     
   cancelAnimationFrame (animationFrameId);
   } 
@@ -143,68 +200,3 @@ document.addEventListener("keyup", event => {
      isAvatarGoingRight = false;
      
 })
-
-
-
-/* SECTION 02 Clone
-
-function blessingOfAMillionBitcoins() {
-  const nextBitcoins = [];
-  bitcoins.forEach(bitcoin => {
-  //const currentStartingPosition = bitcoin.randomStartingPoint();
-  bitcoin.move(); 
-      if (bitcoin.posY < 700) {
-      nextBitcoins.push(bitcoin);
-      const {posY, posX, space, width} = bitcoin
-      ctx.drawImage(bitcoinImg, posX, posY, width, space);
-    }
-  // new: collision logic:
-  if (avatarPositionX < bitcoin.posX + bitcoin.width && avatarPositionX + avatarWidth > bitcoin.posX && avatarPositionY < bitcoin.posY + bitcoin.space &&  avatarLength + avatarPositionY > bitcoin.posY)  {
-  bitcoinCounter +=1;
-  console.log(bitcoinCounter);
-  
- 
- 
-  }
-  });
-
-bitcoins = nextBitcoins;
-}
-
-function startGame () {
-landingPageDiv.classList.add("invisibility");
-animate();
-}
-
-function launchAstronaut () {
-ctx.drawImage(avatarImg, avatarPositionX, avatarPositionY, avatarWidth, avatarLength);
-if (isAvatarGoingLeft) {
-  if (avatarPositionX > 0) {
-    avatarPositionX -= movementSpeed;
-  }
-} else if (isAvatarGoingRight) {
-  if (avatarPositionX < canvasWidth - avatarWidth) {
-    avatarPositionX += movementSpeed;
-  }
-}
-}
-
-function animate() {                                     
-  ctx.clearRect(0, 0, canvasWidth, canvasHeight);
-  ctx.drawImage(backgroundImg, 0, 0, canvas.width, canvas.height);
-  launchAstronaut();
-  blessingOfAMillionBitcoins();
-  if (animationFrameId % 200 === 0) {
-   bitcoins.push(new Bitcoin());
-  } 
-  if (gameOver) {     
-  cancelAnimationFrame (animationFrameId);
-  } 
-    else {
-    animationFrameId = requestAnimationFrame(animate);
-    }
-
-};
-
-
-*/
